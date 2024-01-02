d9e1b8f4509fb77b6db822a0ac6a373fb4192327
Akane Kurosawa <akanekurosawa@fastmail.com>
2024-01-02 09:50:39 -0800

    Redefined arrow keys as modifiers

--- keyboards/preonic/keymaps/akanekurosawa/keymap.c
+++ keyboards/preonic/keymaps/akanekurosawa/keymap.c
@@ -51,68 +51,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-          KC_ESC,    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,   KC_6,   KC_7,  KC_8,       KC_9,     KC_0,  _______,
-          KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y,   KC_U,  KC_I,       KC_O,     KC_P,  KC_BSPC,
-  HYPR_T(KC_F16),    KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H,   KC_J,  KC_K,       KC_L,  KC_SCLN,  KC_QUOT,
-         KC_LSFT,    KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   MT(MOD_RSFT,KC_ENT),
-           MO(6), KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC,  RAISE,  KC_LEFT, KC_DOWN,    KC_UP,  KC_RGHT
+          KC_ESC,    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,   KC_6,   KC_7,  KC_8,       KC_9,    KC_0,  _______,
+          KC_TAB,    KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y,   KC_U,  KC_I,       KC_O,    KC_P,  KC_BSPC,
+  HYPR_T(KC_F16),    KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H,   KC_J,  KC_K,       KC_L, KC_SCLN,  KC_QUOT,
+         KC_LSFT,    KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT, KC_SLSH,  MT(MOD_RSFT,KC_ENT),
+        FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC,  RAISE,  KC_RGUI, KC_RALT, KC_RCTL,  _______
 ),
 
-/* Colemak
+/* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * |   `  |   !  |   @  |   #  |  $   |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * |      |      |      |      |  {   |   }  |      |   7  |   8  |   9  |   =  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |  "   |
+ * |      |      |      |      |  (   |   )  |      |   4  |   5  |   6  |   -  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- *
-[_COLEMAK] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSCP,
-  KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-),
-
- * Dvorak
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Del  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- *
-[_DVORAK] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_DEL,
-  KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT,
-  BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-),
-
- * Lower
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |   {  |   }  |      |   7  |   8  |   9  |   =  |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |   (  |   )  |      |   4  |   5  |   6  |   -  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |   [  |   ]  |      |   1  |   2  |   3  |   \  |      |
+ * |      |      |      |      |  [   |   ]  |      |   1  |   2  |   3  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |   0  |   .  |      |      |
  * `-----------------------------------------------------------------------------------'
  */
+
 [_LOWER] = LAYOUT_preonic_grid(
    KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______,   KC_P7,   KC_P8,   KC_P9,  KC_EQL, _______,
